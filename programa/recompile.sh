203bf8e91b3e88d2ca07f0e74da64c55fd778e77
Rafael Araya <143631166+RafaAraya14@users.noreply.github.com>
2026-01-29 19:06:07 -0600

    Correccion del parser.cup

--- programa/recompile.sh
+++ programa/recompile.sh
@@ -6,7 +6,7 @@ echo "Regenerando Lexer desde lexer.flex..."
 java -jar jflex-1.9.1/lib/jflex-full-1.9.1.jar lexer.flex
 
 echo "Regenerando parser desde parser.cup..."
-java -jar java-cup-11b.jar -expect 2 -parser parser -symbols sym parser.cup
+java -jar java-cup-11b.jar -expect 0 -parser parser -symbols sym parser.cup
 
 echo "Compilando archivos Java..."
 javac -cp ".:java-cup-11b-runtime.jar" *.java
